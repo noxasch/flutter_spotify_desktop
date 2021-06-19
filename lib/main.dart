f04af00857ce10ca58bc1e08818240c642a7ce02
Alexander Dischberg <noxasch@gmail.com>
2021-06-20 02:26:31 +0800

    feat: set riverpod providerScope

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux))
   await DesktopWindow.setMinWindowSize(const Size(800, 600));
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
