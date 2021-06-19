9b299a7724c3bbe1687293d20b6ddc8decb9fe4f
Alexander Dischberg <noxasch@gmail.com>
2021-06-20 02:26:51 +0800

    feat: use currentTrack provider in track list

--- lib/widgets/tracks_list.dart
+++ lib/widgets/tracks_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/models.dart';
 import '../models/song.dart';
+import '../providers/providers.dart';
 
 class TracksList extends StatelessWidget {
   final List<Song> tracks;
@@ -12,36 +15,51 @@ class TracksList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: <DataColumn>[
-        DataColumn(label: Text('title'.toUpperCase())),
-        DataColumn(label: Text('artist'.toUpperCase())),
-        DataColumn(label: Text('album'.toUpperCase())),
-        const DataColumn(label: Icon(Icons.access_time)),
-      ],
-      rows: <DataRow>[
-        ...tracks.map((track) => DataRow(
-          cells: <DataCell>[
-            DataCell(
-              Text(track.title, style: TextStyle(
-                color: Theme.of(context).iconTheme.color),)
-            ),
-            DataCell(
-              Text(track.artist, style: TextStyle(
-                color: Theme.of(context).iconTheme.color),)
-            ),
-            DataCell(
-              Text(track.album, style: TextStyle(
-                color: Theme.of(context).iconTheme.color),)
-            ),
-            DataCell(
-              Text(track.duration, style: TextStyle(
-                color: Theme.of(context).iconTheme.color),)
-            ),
-            
-          ]
-        )).toList()
-      ],
+    return Consumer(
+      builder: (context, watch, child) {
+        final CurrentTrack currentTrack = watch(currentTrackProvider);
+        return DataTable(
+          headingTextStyle: Theme.of(context).textTheme.overline!.copyWith(
+            fontSize: 12.0
+          ),
+          dataRowHeight: 54.0,
+          showCheckboxColumn: false,
+          columns: <DataColumn>[
+            DataColumn(label: Text('title'.toUpperCase())),
+            DataColumn(label: Text('artist'.toUpperCase())),
+            DataColumn(label: Text('album'.toUpperCase())),
+            const DataColumn(label: Icon(Icons.access_time)),
+          ],
+          rows: <DataRow>[
+            ...tracks.map((track) {
+              final bool selected = currentTrack.selectedSong?.id == track.id;
+              final TextStyle style =  
+                TextStyle(color: selected ? Theme.of(context).accentColor 
+                  : Theme.of(context).iconTheme.color);
+              return DataRow(      
+                cells: <DataCell>[
+                  DataCell(
+                    Text(track.title, style: style,)
+                  ),
+                  DataCell(
+                    Text(track.artist, style: style,)
+                  ),
+                  DataCell(
+                    Text(track.album, style: style,)
+                  ),
+                  DataCell(
+                    Text(track.duration, style: style,)
+                  ),
+                ],
+                selected: selected,
+                onSelectChanged: (_) {
+                  context.read(currentTrackProvider.notifier).selectTrack(track);
+                }
+              );
+            }).toList()
+          ],
+        );
+      },
     );
   }
 }
